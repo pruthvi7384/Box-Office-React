a701edba4f05f05eca514fa2aa34ce2440043024
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-08-24 17:02:54 +0530

    define-Theme-from styled Componenets

--- src/Components/Navs.js
+++ src/Components/Navs.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { NavList,LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
@@ -7,15 +8,19 @@ const LINKS = [
 ];
 
 const Navs = () => {
+  const location =useLocation();
+  console.log('location',location);
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+            className={item.to===location.pathname ? 'active' : ''}
+            to={item.to}>{item.text}</LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
