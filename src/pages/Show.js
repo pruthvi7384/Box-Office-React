8857fb8cbae7fbc8a79d35603fae86e3c91346f2
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-08-24 12:27:38 +0530

    replace usestate to useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,27 +1,46 @@
-import React,{ useState, useEffect } from 'react'
+import React,{ useReducer, useEffect } from 'react'
 import {useParams} from 'react-router-dom';
 import {apiget} from '../mics/config';
 
+const reducer =(prevstate,action) => {
+    switch(action.type) {
+
+        case 'FEATCH_SUCCESS':{
+            return {isloding:false,iserror:null,show:action.show};
+        }
+        case 'FEATCH_FAILED': {
+            return {...prevstate,isloding:false,iserror:action.iserror};
+        }
+        default: return prevstate
+    }
+}
+
+const initialState = {
+    show:null,
+    isloding:true,
+    iserror:null
+}
+
 function Show() {
     const {id} = useParams();
-    const [show,setShow]=useState(null);
-    const [isloding,setLoding]=useState(true);
-    const [iserror,setError]=useState(null);
+    // const [show,setShow]=useState(null);
+    // const [isloding,setLoding]=useState(true);
+    // const [iserror,setError]=useState(null);
     // console.log(id);
+    const [{show,isloding,iserror},despatch] =useReducer(reducer,initialState);
+    
     useEffect(()=>{
         let isMounted = true;
         apiget(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(results => {
             setTimeout(()=>{
                 if(isMounted) {
-                    setShow(results);
-                    setLoding(false);
+                    despatch({type:'FEATCH_SUCCESS',show:results})
                 }
             },2000)
         }).catch(err => {
             if(isMounted) {
-                setError(err.message);
-                setLoding(false);
+                despatch({type:'FEATCH_FAILED',iserror:err.message})
             }
         })
         return ()=>{
