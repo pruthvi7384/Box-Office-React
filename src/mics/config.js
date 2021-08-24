4eae4f4001374e49f2cad9b3544fe1386f540613
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-08-24 11:25:17 +0530

    added loding and error messages

--- src/mics/config.js
+++ src/mics/config.js
@@ -3,5 +3,6 @@ const API_BASE_URL =  'https://api.tvmaze.com';
 
 export async function apiget(queryString){
     const response = await fetch(`${API_BASE_URL}${queryString}`).then(r=>r.json());
+    // throw new Error('Oppps')
     return response;
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,14 +5,36 @@ import {apiget} from '../mics/config';
 function Show() {
     const {id} = useParams();
     const [show,setShow]=useState(null);
+    const [isloding,setLoding]=useState(true);
+    const [iserror,setError]=useState(null);
     // console.log(id);
     useEffect(()=>{
+        let isMounted = true;
         apiget(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(results => {
-            setShow(results);
+            setTimeout(()=>{
+                if(isMounted) {
+                    setShow(results);
+                    setLoding(false);
+                }
+            },2000)
+        }).catch(err => {
+            if(isMounted) {
+                setError(err.message);
+                setLoding(false);
+            }
         })
+        return ()=>{
+            isMounted= false;
+        }
     }, [id] )
     console.log(show);
+    if(isloding){
+        return <div>Data Is being Loded</div>
+    }
+    if(iserror){
+        return <div>Error Occured : {iserror}</div>
+    }
     return (
         <div>
             This is show page
