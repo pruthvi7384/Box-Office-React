03ea67ae4ac1abbcacb4462944e028e77ab2caca
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-08-24 17:53:30 +0530

    Updates

---
+++ precache-manifest.9aef3b5414cf3c5a9475dfe96bf8cd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a3ce3ce15d38de2d1327a3b2a94f1f",
+    "url": "/Box-Office-React/index.html"
+  },
+  {
+    "revision": "0c2be9a3d2f639f8414e",
+    "url": "/Box-Office-React/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "1ef137c28767564f7025",
+    "url": "/Box-Office-React/static/js/2.d1837cd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office-React/static/js/2.d1837cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2be9a3d2f639f8414e",
+    "url": "/Box-Office-React/static/js/main.f074dbf5.chunk.js"
+  },
+  {
+    "revision": "9338f091dccd577ccfcb",
+    "url": "/Box-Office-React/static/js/runtime-main.17046b96.js"
+  }
+]);
